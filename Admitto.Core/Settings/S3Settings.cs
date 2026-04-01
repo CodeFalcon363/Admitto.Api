7988c1b9774fd1be220e90bfe5163b149530175c
user.name <anthonyoise@gmail.com>
2026-04-01 19:42:44 +0100

    feat(core): add S3Settings and make IFileService.Delete async

---
+++ Admitto.Core/Settings/S3Settings.cs
@@ -0,0 +1,11 @@
+namespace Admitto.Core.Settings
+{
+    public class S3Settings
+    {
+        public string BucketName { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public string AccessKey { get; set; } = null!;
+        public string SecretKey { get; set; } = null!;
+        public string BaseUrl { get; set; } = null!;
+    }
+}
--- Admitto.Infrastructure/Interfaces/IServices/IFileService.cs
+++ Admitto.Infrastructure/Interfaces/IServices/IFileService.cs
@@ -3,6 +3,6 @@ namespace Admitto.Infrastructure.Interfaces.IServices
     public interface IFileService
     {
         Task<string> SaveAsync(Stream fileStream, string fileName, string folder);
-        void Delete(string relativePath);
+        Task DeleteAsync(string fileUrl);
     }
 }
